dd739ec53d6b2d4b99e14608bdef9634a58595c7
adelson70 <bittencourtadelson1@gmail.com>
2024-08-09 18:05:35 -0300

    preparativos

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
 from flask import Flask
+from socket import gethostbyname, gethostname
+from routes import configure_routes
+
+
+app = Flask(__name__)
+
+# CHAVE DO COOKIE
+app.secret_key = '7c9b4342db84949350c9e19ee4f72ac7cc47c7afa477f5b2b9fc9bb8401c0aa0'
+
+if __name__ == '__main__':
+    configure_routes(app)
+
+    # NOME DO COMPUTADOR
+    nome_host = gethostname()
+    # ENDEREÇO DE IPV4 DO COMPUTADOR
+    ip_host = gethostbyname(nome_host)
+
+    app.run(debug=True, host=ip_host)
--- routes.py
+++ routes.py
@@ -1,1 +1,7 @@
 # GERENCIAMENTO DAS ROTAS
+from flask import render_template, redirect, url_for, request, session
+def configure_routes(app):
+
+    @app.route('/', methods=['POST','GET'])
+    def inicio():
+        return(render_template('index.html'))

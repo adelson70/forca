bc345714fa1607f9d6a0e6897418ec61f5e6e77c
adelson70 <bittencourtadelson1@gmail.com>
2024-08-11 17:12:41 -0300

    criando estrutura

--- database.py
+++ database.py
@@ -1,1 +1,32 @@
 # BANCO DE DADOS
+import sqlite3 as sql
+
+# FUNÇÃO PARA CONECTAR O BANCO DE DADOS
+# IRA CRIAR AS TABELAS CASO NÃO EXISTAM
+def conectarDB():
+    conexao = sql.connect('database/forca.db')
+    cursor = conexao.cursor()
+
+    # CRIANDA TABELA ONDE IRA ARMAZENAR AS PALAVRAS CHAVES
+    cursor.execute('''
+                   CREATE TABLE IF NOT EXISTS palavras(
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   palavra VARCHAR(30))
+                   ''')
+
+    # CRIANDA A TABELA PARA VALIDAR O USUARIO ADMIN
+    cursor.execute('''
+                   CREATE TABLE IF NOT EXISTS user(
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   nome_usuario VARCHAR(10),
+                   senha_usuario VARCHAR(10))
+                   ''')
+    
+    conexao.commit()
+
+
+    return conexao, cursor
+
+if __name__ == '__main__':
+    conectarDB()
+    print('banco carregado')
--- routes.py
+++ routes.py
@@ -2,6 +2,9 @@
 from flask import render_template, redirect, url_for, request, session
 def configure_routes(app):
 
+    # ROTA DE INICIO DA APLICAÇÃO
     @app.route('/', methods=['POST','GET'])
     def inicio():
-        return(render_template('index.html'))
+        return(render_template('forca.html'))
+    
+    # ROTA PARA ...
